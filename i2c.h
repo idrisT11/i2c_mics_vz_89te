90dd4fccc96625340b50fb0c9b5f17d485b129de
idris <64688223+idrisT11@users.noreply.github.com>
2021-11-03 11:36:08 +0100

    Update i2c.h

--- i2c.h
+++ i2c.h
@@ -14,11 +14,12 @@
 
 
 void InitI2C0(void);
-void  I2CReceiveN(uint8_t slave_addr, uint8_t reg, uint8_t numElem, uint8_t buff[]);
+void InitI2C1(void);
+void InitI2C2(void);
+void  I2CReceiveN(uint8_t port, uint8_t slave_addr, uint8_t reg, uint8_t numElem, uint8_t buff[]);
 uint32_t I2CReceive(uint32_t slave_addr, uint8_t reg);
 void I2CSend(uint8_t slave_addr, uint8_t num_of_args, ...);
-void I2CSend_String(uint32_t slave_addr, char array[]);
-
+                                     
 
 
 
